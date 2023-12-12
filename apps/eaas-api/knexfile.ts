e638ba69f00e8b7f27323ddd7e87b35c3f0195b6
Kellar <kellar.base09@gmail.com>
2023-12-12 13:05:55 +0000

    Use envConfig instead in knexfile config

--- apps/eaas-api/knexfile.ts
+++ apps/eaas-api/knexfile.ts
@@ -1,28 +1,8 @@
-import dotenv from "dotenv";
-import Joi from "joi";
-import path from "path";
+import { envConfig } from "./src/config/env.config";
 
-dotenv.config();
-
-if (process.env.NODE_ENV === "test") {
-  dotenv.config({ path: path.join(__dirname, "./.env.test") });
-}
-
-const dbUrlSchema = Joi.object()
-  .keys({
-    DATABASE_URL: Joi.string().required().description("database connection url"),
-  })
-  .unknown();
-
-const { value: env, error } = dbUrlSchema.prefs({ errors: { label: "key" } }).validate(process.env);
-
-if (error) {
-  throw new Error(`Environment variable validation error: ${error.message}`);
-}
-
-// TODO: Use envConfig instead. For some reason, its import is currently not working.
-const DATABASE_URL = env.DATABASE_URL;
+const DATABASE_URL = envConfig.databaseUrl;
 const DATABASE_URL_TEST = `${DATABASE_URL}-test`;
+
 const MIGRATIONS_DIR = "./src/database/migrations";
 const SEEDS_DIR = "./src/database/seeds";
 
