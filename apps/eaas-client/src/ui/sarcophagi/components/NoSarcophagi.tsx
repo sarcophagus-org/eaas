26270d1e0e1ce33865db279d3a469ff19978e6f9
Kellar <kellar.base09@gmail.com>
2023-12-20 13:32:57 +0000

    don't show create sarco for lawyer

--- apps/eaas-client/src/ui/sarcophagi/components/NoSarcophagi.tsx
+++ apps/eaas-client/src/ui/sarcophagi/components/NoSarcophagi.tsx
@@ -1,7 +1,8 @@
 import { Button, Center, CenterProps, Text, VStack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-import React from "react";
 import { RouteKey, RoutesPathMap } from "../../routerConstants";
+import { useSelector } from "store";
+import { UserType } from "types/userTypes";
 
 // Component that shows when there are no sarcophagi to display on the sarcohagi component
 export function NoSarcpohagi(props: CenterProps) {
@@ -10,15 +11,17 @@ export function NoSarcpohagi(props: CenterProps) {
   function handleClickCreate() {
     navigate(RoutesPathMap[RouteKey.CLIENT_DASHBOARD_PAGE]);
   }
+const {user} = useSelector(s => s.userState);
+  
   return (
     <Center border="1px solid" borderColor="whiteAlpha.300" py={8} {...props}>
       <VStack>
         <Text fontSize="xl" fontWeight="bold">
           You have no sarcophagi
         </Text>
-        <Button variant="link" onClick={handleClickCreate}>
+        {user?.type === UserType.client && <Button variant="link" onClick={handleClickCreate}>
           Create a Sarcophagus
-        </Button>
+        </Button>}
       </VStack>
     </Center>
   );
